3e415dce6cb688a9b5ac220640325637979d0524
arda2626 <78620684+arda2626@users.noreply.github.com>
2025-11-04 21:42:40 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,18 @@
-# AI key sadece env üzerinden
+# config.py
 import os
 
-AI_API_KEY = os.getenv("AI_API_KEY", "")
+# Sadece AI Key env olarak
+AI_KEY = os.getenv("AI_KEY", "")
 
-# Sabit API keyler
-API_FOOTBALL_KEY = "bd1350bea151ef9f56ed417f0c0c3ea2"
-THE_ODDS_API_KEY = "501ea1ade60d5f0b13b8f34f90cd51e6"
-FOOTYSTATS_KEY = "test85g57"
-ALLSPORTSAPI_KEY = "27b16a330f4ac79a1f8eb383fec049b9cc0818d5e33645d771e2823db5d80369"
-SPORTSMONKS_KEY = "AirVTC8HLItQs55iaXp9TnZ45fdQiK6ecwFFgNavnHSIQxabupFbTrHED7FJ"
-ISPORTSAPI_KEY = "rCiLp0QXNSrfV5oc"
+# Sabit API anahtarları
+API_KEYS = {
+    "API_FOOTBALL": "bd1350bea151ef9f56ed417f0c0c3ea2",
+    "THE_ODDS_API": "501ea1ade60d5f0b13b8f34f90cd51e6",
+    "FOOTYSTATS": "test85g57",
+    "ALLSPORTSAPI": "27b16a330f4ac79a1f8eb383fec049b9cc0818d5e33645d771e2823db5d80369",
+    "SPORTSMONKS": "AirVTC8HLItQs55iaXp9TnZ45fdQiK6ecwFFgNavnHSIQxabupFbTrHED7FJ",
+    "ISPORTSAPI": "rCiLp0QXNSrfV5oc"
+}
 
-# Telegram bot token env üzerinden
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "")
-CHAT_ID = "@stakedrip"  # kanal ID veya kullanıcı adı
+# Lokal saat farkı
+TIME_DELTA = 3  # UTC+3 Türkiye
