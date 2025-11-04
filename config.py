1cae618e6e10bad4b5745b214c86c4f9748b2c18
arda2626 <78620684+arda2626@users.noreply.github.com>
2025-11-04 21:35:44 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,16 @@
-# config.py
-AI_API = os.getenv("AI_API")  # Sadece AI key env üzerinden alınacak
+# AI key sadece env üzerinden
+import os
+
+AI_API_KEY = os.getenv("AI_API_KEY", "")
+
+# Sabit API keyler
+API_FOOTBALL_KEY = "bd1350bea151ef9f56ed417f0c0c3ea2"
+THE_ODDS_API_KEY = "501ea1ade60d5f0b13b8f34f90cd51e6"
+FOOTYSTATS_KEY = "test85g57"
+ALLSPORTSAPI_KEY = "27b16a330f4ac79a1f8eb383fec049b9cc0818d5e33645d771e2823db5d80369"
+SPORTSMONKS_KEY = "AirVTC8HLItQs55iaXp9TnZ45fdQiK6ecwFFgNavnHSIQxabupFbTrHED7FJ"
+ISPORTSAPI_KEY = "rCiLp0QXNSrfV5oc"
+
+# Telegram bot token env üzerinden
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "")
+CHAT_ID = "@stakedrip"  # kanal ID veya kullanıcı adı
